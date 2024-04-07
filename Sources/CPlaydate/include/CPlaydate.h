c00a64e9eabe775a370a8cfa8074caa62b66b529
pcbeard <pcbeard@mac.com>
2024-04-07 10:03:02 -0700

    Add errorToConsole() which colorizes messages red

--- Sources/CPlaydate/include/CPlaydate.h
+++ Sources/CPlaydate/include/CPlaydate.h
@@ -4,3 +4,7 @@
 static inline void logToConsole(const struct playdate_sys* system, const char *message) {
     system->logToConsole("%s", message);
 }
+
+static inline void errorToConsole(const struct playdate_sys* system, const char *message) {
+    system->error("%s", message);
+}
